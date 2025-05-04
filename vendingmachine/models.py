4c9a5b031789e22babcea4ede71113638f6d0a2b
(_S_K_) <tssathya26@gmail.com>
2025-05-05 00:39:08 +0530

    INITIAL

--- vendingmachine/models.py
+++ vendingmachine/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class vending_machine(models.Model):
+    vending_machine_type = models.CharField(max_length=100)
+    no_of_items = models.IntegerField(db_default=0)
+
+class machine_item(models.Model):
+    item_name = models.ForeignKey(vending_machine, on_delete=models.CASCADE)
+    item_cost = models.FloatField()
+
+
