377e070dfc3dd87b486fdc8c5428b74728458b8e
(_S_K_) <tssathya26@gmail.com>
2025-05-07 19:24:44 +0530

    views, models, admin

--- vendingmachine/admin.py
+++ vendingmachine/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Vending_machine
+
+admin.site.register(Vending_machine)
--- vendingmachine/views.py
+++ vendingmachine/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from .models import Vending_machine, machine_item
 
 def Home(request):
-    list_of_machines = Vending_machine.objects.order_by("-machine_id")
+    list_of_machines = Vending_machine.objects.order_by("machine_id")
     context = {"list_of_machines" : list_of_machines}
     return render(request, "vendingmachine/home.html", context)
 
 def Machine(request, id):
     machine = get_object_or_404(Vending_machine, machine_id = id)
-    return render(request, 'vendingmachine/vm.html')
+    return render(request, 'vendingmachine/vm.html', {'machine' : machine})
