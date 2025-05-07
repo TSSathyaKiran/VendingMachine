68bff2ece779bac485b4c5e4775dc257ab344120
(_S_K_) <tssathya26@gmail.com>
2025-05-07 18:59:54 +0530

    views, models

--- vendingmachine/models.py
+++ vendingmachine/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class vending_machine(models.Model):
+class Vending_machine(models.Model):
+    machine_id = models.CharField(max_length=4, unique=True)
     vending_machine_type = models.CharField(max_length=100)
     no_of_items = models.IntegerField(db_default=0)
 
+    def items_availablity(self):
+        if self.no_of_items < 1:
+            return False
+
 class machine_item(models.Model):
-    item_name = models.ForeignKey(vending_machine, on_delete=models.CASCADE)
+    vending_machine = models.ForeignKey(Vending_machine, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=100)
     item_cost = models.FloatField()
+    item_quantity = models.IntegerField()
 
-
+    def purchase(self):
+        if self.item_quantity > 0:
+            self.item_quantity -= 1
+            return True
+        else:
+            return False
+    
--- vendingmachine/views.py
+++ vendingmachine/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Vending_machine, machine_item
 
-# Create your views here.
+def Home(request):
+    list_of_machines = Vending_machine.objects.order_by("-machine_id")
+    context = {"list_of_machines" : list_of_machines}
+    return render(request, "vendingmachine/home.html", context)
+
+def Machine(request, id):
+    machine = get_object_or_404(Vending_machine, machine_id = id)
+    return render(request, 'vendingmachine/vm.html')
