76f21843f62653b5f94bb4bc6bb11ff3df13e349
(_S_K_) <tssathya26@gmail.com>
2025-05-21 22:54:30 +0530

    authentication

--- vendingmachine/views.py
+++ vendingmachine/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Vending_machine, machine_item
+from django.contrib.auth import authenticate, login, logout
 
+def user_login(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username = username, password = password)
+    if user is not None:
+        login(request, user)
+        render()
+
+        
 def Home(request):
     list_of_machines = Vending_machine.objects.order_by("machine_id")
     context = {"list_of_machines" : list_of_machines}
